559e04f1d7bbaf21a0a49f20ccc8e7a76f1bd446
GeoffedUP <35282584+GeoffedUP@users.noreply.github.com>
2018-12-09 19:45:14 -0800

    wrapped bloxy.info api allorigins.ml
    -removed whateverorigin.org wrap
    -change sidebar height to work with microsoft edge
    -commented http://api.0xbtc.io tokenStats

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -15,17 +15,17 @@ const main = require('./template/main');
 import Fundraiser from './template/fundraiser'
 
 const mineable_token = require('./template/mineable-token-vue.js');
-import TokenStats from './template/tokenStats'
+//import TokenStats from './template/tokenStats'
 
 
 let fundraiser = new Fundraiser(Eth);
-let tokenStats = new TokenStats();
+//let tokenStats = new TokenStats();
 
 $(document).ready(function() {
   console.log("DOMready");
 
   fundraiser.init()
 
-  tokenStats.init();
+//  tokenStats.init();
 
 });
